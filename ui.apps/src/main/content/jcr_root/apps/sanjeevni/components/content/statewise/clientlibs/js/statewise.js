5879f5a97c588f5f22869bbf81dd8ad22b469be9
ShubhamMittal <shubhamm@hcl.com>
2020-08-08 21:38:35 +0530

    sorting functionality added.

--- ui.apps/src/main/content/jcr_root/apps/sanjeevni/components/content/statewise/clientlibs/js/statewise.js
+++ ui.apps/src/main/content/jcr_root/apps/sanjeevni/components/content/statewise/clientlibs/js/statewise.js
@@ -31,53 +31,75 @@ $(document).ready(function() {
 		}
 		
 		
+		function sortByProperty(property){
+		   return function(a,b){
+			return b[property] - a[property];
+			 /* if(a[property] > b[property])
+				 return 1;
+			  else if(a[property] < b[property])
+				 return -1;
+
+			  return 0;  */
+		   }
+		}
+
+
 		async function fetchCovidReports2() {
-			
+
 			    const stateresponse = await fetch('https://api.covid19india.org/data.json');
                 const mainResponse = await stateresponse.json();
                 const stateWiseInfo = mainResponse.statewise;
-   
+
+
+				stateWiseInfo.sort(sortByProperty("confirmed"));
+
+
+
                 const response = await fetch('https://api.covid19india.org/v5/min/data.min.json');
                 const recordSet = await response.json();
 				let data="";
 
 				for (var key in stateWiseInfo) {
-					
+
 					rowClass="odd";
 					if(key%2){
 						rowClass="even";
 					}
 					if(stateWiseInfo[key].statecode=="TT" ){
-					document.querySelector('.confirmed').innerHTML=numberformat(stateWiseInfo[0].confirmed);
-					document.querySelector('.active').innerHTML=numberformat(stateWiseInfo[0].active);
-					document.querySelector('.recovered').innerHTML=numberformat(stateWiseInfo[0].recovered);
-					document.querySelector('.deceased').innerHTML=numberformat(stateWiseInfo[0].deaths);
-					} else {
+						document.querySelector('.confirmed').innerHTML=numberformat(stateWiseInfo[0].confirmed);
+						document.querySelector('.active').innerHTML=numberformat(stateWiseInfo[0].active);
+						document.querySelector('.recovered').innerHTML=numberformat(stateWiseInfo[0].recovered);
+						document.querySelector('.deceased').innerHTML=numberformat(stateWiseInfo[0].deaths);
+					}
+					else if(stateWiseInfo[key].statecode=="UN" || stateWiseInfo[key].statecode=="LD"){
+						//
+					}
+					else {
 						if(document.getElementById( "IN-"+stateWiseInfo[key].statecode )!==null){
 						document.getElementById( "IN-"+stateWiseInfo[key].statecode ).setAttribute("fill", mapColorCase(stateWiseInfo[key].confirmed));
 						}
-						
+
 						data +="<tr class=\"clickable js-tabularinfo-toggle cursor-pointer list-of-states state-filter "+rowClass+" \" data-state=\"IN-"+stateWiseInfo[key].statecode+"\" data-toggle=\"collapse\"   id="+stateWiseInfo[key].statecode+ " data-target=."+stateWiseInfo[key].statecode+"><td width=\"20%\"><strong>"+stateWiseInfo[key].state+"</strong></td><td width=\"20%\">"+checkUndefined(stateWiseInfo[key].confirmed)+"</td><td width=\"20%\">"+checkUndefined(stateWiseInfo[key].deaths)+"</td><td width=\"20%\">"+checkUndefined(stateWiseInfo[key].recovered)+"</td><td width=\"20%\">"+checkUndefined(stateWiseInfo[key].active)+"</td></tr>";
 						if(stateWiseInfo[key].confirmed!=0) {
-							data+="<tr class=\"tabularinfo__subblock collapse "+stateWiseInfo[key].statecode+"\"><td colspan=\"5\" ><table id=\"\" class=\"tabularinfo tabularinfo--child list-of-states \" data-state=\"IN-"+stateWiseInfo[key].statecode+"\" data-detail-view=\"\" data-set=\""+stateWiseInfo[key].state+"-"+numberformat(stateWiseInfo[key].confirmed)+"-"+numberformat(stateWiseInfo[key].deaths)+"-"+numberformat(stateWiseInfo[key].recovered)+"\"><thead><tr><th data-field=\"\" class=\"pl-2\">District</th><th class=\"pl-2\" data-field=\"\">Confirmed</th><th data-field=\"\" class=\"pl-2\" >Deceased</th><th data-field=\"\" class=\"pl-2\">Recovered</th><th data-field=\"\" class=\"pl-2\">Active</th></tr></thead><tbody>";
+							data+="<tr  class=\"tabularinfo__subblock collapse "+stateWiseInfo[key].statecode+"\"><td colspan=\"5\" ><table id=\"\" class=\"tabularinfo tabularinfo--child list-of-states \" data-state=\"IN-"+stateWiseInfo[key].statecode+"\" data-detail-view=\"\" data-set=\""+stateWiseInfo[key].state+"-"+numberformat(stateWiseInfo[key].confirmed)+"-"+numberformat(stateWiseInfo[key].deaths)+"-"+numberformat(stateWiseInfo[key].recovered)+"\"><thead><tr><th data-field=\"\" class=\"pl-2\" >District</th><th class=\"pl-2\" data-field=\"\">Confirmed</th><th data-field=\"\" class=\"pl-2\" >Deceased</th><th data-field=\"\" class=\"pl-2\">Recovered</th><th data-field=\"\" class=\"pl-2\">Active</th></tr></thead><tbody>";
 							for (var distKey in recordSet[stateWiseInfo[key].statecode].districts){
-								
+
 								data +="<tr class=\"subrow1\" data-href=\"#\"><td width=\"20%\"><strong>"+distKey+"</strong></td><td width=\"20%\">"+checkUndefined(recordSet[stateWiseInfo[key].statecode].districts[distKey].total.confirmed)+"</td><td width=\"20%\">"+checkUndefined(recordSet[stateWiseInfo[key].statecode].districts[distKey].total.deceased)+"</td><td width=\"20%\">"+checkUndefined(recordSet[stateWiseInfo[key].statecode].districts[distKey].total.recovered)+"</td><td width=\"20%\">"+getActive(recordSet[stateWiseInfo[key].statecode].districts[distKey].total.confirmed,recordSet[stateWiseInfo[key].statecode].districts[distKey].total.recovered,recordSet[stateWiseInfo[key].statecode].districts[distKey].total.deceased)+"</td></tr>";
 							}
 							data +="</tbody></table></td></tr>";
 						}
 					}
-					
+
 				}
-				
-				
-				
-				
+
+
+
+
 				document.querySelector('#covidData').innerHTML=data;
 				wordStates = document.querySelectorAll(".list-of-states");
 				wordStates.forEach(function(el) {
 				el.addEventListener("mouseenter", function(e) {
-				
+
 				addOnFromList(el,e,"table");
 				});
 				el.addEventListener("mouseleave", function() {
@@ -106,11 +128,12 @@ $(document).ready(function() {
 
 
 				}
-		
-					fetchCovidReports2();
-		
-		
-		
+
+
+ fetchCovidReports2();
+
+
+
 
 function removeAllOn() {
   wordStates.forEach(function(el) {
@@ -123,7 +146,7 @@ function removeAllOn() {
 	el.classList.remove("medium");
 	el.classList.remove("mild");
   });
-  
+
    $('.info_panel').remove();
 }
 
@@ -148,23 +171,23 @@ function addOnFromList(el,e,eventType) {
 	recoverCase=childs[2].innerHTML;
 	deceaseCase=childs[3].innerHTML;
  }
-  
+
 
   var svgState = document.querySelector("#" + stateCode);
   //el.classList.add("on");
-  
-  
-  
-  
+
+
+
+
   svgState.classList.add("on");
-  
+
 
   $('<div class="info_panel"><strong>State:</strong>'+stateName+'<br><strong>Confirmed</strong>:'+confirmCase+'<br><strong>Deceased</strong>:'+recoverCase+'<br><strong>Recovered</strong>:'+deceaseCase+'</div>').appendTo('body');
 //  console.log(stateName);
  if(eventType=="map"){
 	   $('.info_panel').css({
             top: e.clientY,
-            left: e.clientX 
+            left: e.clientX
    });
  }
  else
@@ -256,17 +279,19 @@ function getActive(active,recover,deceased){
 				}
 				}
 			}
-   
+
 		});
-		
-		
+
+
 	 $('.js-tabularinfo').bootstrapTable({
         escape: false,
         showHeader: false
       });
-		
-		
-		
+
+
+
+
+
 	
 
     
